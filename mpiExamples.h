8025c5c693a7409e9e39c4a6b486a606a54c5264
Aimene Amiour <aimeneamiour@gmail.com>
2018-12-02 14:59:17 +0100

    program 1 done

---
+++ mpiExamples.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef MPIEXAMPLECLASS_H
+#define MPIEXAMPLECLASS_H
+
+
+class mpiexamples {
+
+public : 
+
+	void program1(int argc, char * argv[]);
+};
+#endif // !MPIEXAMPLECLASS_H
