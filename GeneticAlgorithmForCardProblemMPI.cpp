a6a521c5609f0c1cb2b8256e3ec9d74eb7d296f1
Aimene Amiour <aimeneamiour@gmail.com>
2019-01-08 08:23:21 +0100

    code is clean

--- GeneticAlgorithmForCardProblemMPI.cpp
+++ GeneticAlgorithmForCardProblemMPI.cpp
@@ -9,10 +9,9 @@
 
 
 GeneticAlgorithmForCardProblemMPI::GeneticAlgorithmForCardProblemMPI(int maxiteration, int popsize, int dim, double probCross,
-	double probaMut, double sumTrag, double prodTrag, int number_of_individual_to_send, int number_of_iteration_before_sending):
+	double probaMut, double sumTrag, double prodTrag):
 	_max_iterations{ maxiteration }, _popsize{ popsize }, _dimension{ dim }, _crossover_probability{ probCross },
-	_mutation_probability{ probaMut }, _sum_targ{ sumTrag }, _prod_targ{ prodTrag },
-	_number_of_individual_to_send{ number_of_individual_to_send }, _number_of_iteration_before_sending{ number_of_iteration_before_sending }
+	_mutation_probability{ probaMut }, _sum_targ{ sumTrag }, _prod_targ{ prodTrag }
 
 {
 
@@ -20,13 +19,6 @@ GeneticAlgorithmForCardProblemMPI::GeneticAlgorithmForCardProblemMPI(int maxiter
 		for (int i = 0; i < _popsize; i++)
 			_population[i] = new int[_dimension];
 
-	/*_population = (int**)malloc(_popsize * sizeof(int));
-	for (int i = 0; i < _popsize; i++) {
-		_population[i] = (int*)malloc(_dimension * sizeof(int));
-		std::cout << i << " , ";
-	}*/
-   //	_population[0][0]= 12;
-	//std::cout << _population[0][0];
 }
 
 GeneticAlgorithmForCardProblemMPI::~GeneticAlgorithmForCardProblemMPI()
@@ -39,25 +31,92 @@ GeneticAlgorithmForCardProblemMPI::~GeneticAlgorithmForCardProblemMPI()
 	
 	
 }
-
-double GeneticAlgorithmForCardProblemMPI::evaluate(int n)
+void GeneticAlgorithmForCardProblemMPI::display(int iteration, int individual)
 {
-	int sum = 0; int prod = 0;
-	double scaled_sum_error, scaled_prod_error, combined_error;
+	std::cout << "==================================" << std::endl;
+	std::cout << " after " << iteration << " Iterations, Solution sum pile ( should be 36 ) card are :" << std::endl;
+	int sum = 0;
+	int prod = 1;
+	for (int i = 0; i < _dimension; ++i)
+	{
+
+		if (_population[individual][i] == 0)
+		{
+			std::cout << (i + 1) << "+";
+			std::cout << sum;
+
+			sum += (i + 1);
+		}
+		std::cout << "=" << sum << std::endl;
+	}
+	std::cout << " and Product pile ( should be 360 ) cards are :" << std::endl;
 
 	for (int i = 0; i < _dimension; ++i)
 	{
-		if (_population[n][i]==0)
+
+		if (_population[individual][i] == 1)
 		{
-			sum += (1 + i);
-		}
-		else {
+			std::cout << (i + 1) << "*";
+			std::cout << prod;
 			prod *= (i + 1);
+
 		}
+		std::cout << "=" << prod << std::endl;
+	}
+
+}
+
+//initialise population
+void GeneticAlgorithmForCardProblemMPI::initial_population()
+{
+	//for entire population
+	for (int i = 0; i < _popsize; ++i)
+	{
+		//for all genes
+		for (int j = 0; j < _dimension; ++j)
+		{
+			double x = random(0, 1);
+			//randomly create gene values
+			if (random(0, 2) < 0.5)
+				_population[i][j] = 0;
+			else
+				_population[i][j] = 1;
+		}
+	}
+
+}
+
+double GeneticAlgorithmForCardProblemMPI::random(double min, double max) const
+{
+
+	double f = (double)rand() / RAND_MAX;
+	return min + f * (max - min);
+
+}
+
+
+double GeneticAlgorithmForCardProblemMPI::evaluate(int n)
+{	           
+	//initialise field values
+	int sum = 0; int prod = 1;
+	double scaled_sum_error, scaled_prod_error, combined_error;
+	//loop though all genes for this population member
+	for (int i = 0; i < _dimension; ++i)
+	{
+		//if the gene value is 0, then put it in the sum (pile 0), and calculate sum
+		if (_population[n][i]==0)
+			sum += (1 + i);
+		//if the gene value is 1, then put it in the product (pile 1), and calculate sum
+		else 
+			prod *= (i + 1);
+	
 
 	}
+	//work out how food this population member is, based on an overall error
+	//for the problem domain
+	//NOTE : The fitness function will change for every problem domain.
 	scaled_sum_error = (sum - _sum_targ) / _sum_targ;
-	scaled_prod_error = (prod - -_prod_targ) / _prod_targ; 
+	scaled_prod_error = (prod - _prod_targ) / _prod_targ; 
 
 	combined_error = abs(scaled_sum_error) + abs(scaled_prod_error);
 	return combined_error;
@@ -65,43 +124,63 @@ double GeneticAlgorithmForCardProblemMPI::evaluate(int n)
 
 void GeneticAlgorithmForCardProblemMPI::run(int argc, char * argv[])
 {
+
 	int num_procs, my_rank;
+
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	MPI_Status my_status;
 
-	std::cout << " run begin " << std::endl;
+	
 	int a, b, winner, loser;
+	//initialize the population (randomly)
 	initial_population();
 
-
+	// 2 vector for the 2 best population that will be send
 	int * best_pop1;
 	int * best_pop2;
 
-	int tag = 123;
-
-
-	double best1, best2, test;
-	double worst1, worst2;
-
-	int indiceBest1, indiceBest2;
-	int indiceWorst1, indiceWorst2;
-
-	
-	
-
+	// initialize vector
 	best_pop1 = new int[11];
 	best_pop2 = new int[11];
 
+	int tag = 123;
 
+	// the previous process
+	int num_proc_precedent = (num_procs + my_rank - 1) % num_procs;
 
+	// the next process
+	int num_proc_suivant = (my_rank + 1) % num_procs;
 
+	// fitness values 
+	double best1, best2, test;
+	double worst1, worst2;
+
+	//population indexes
+	int indiceBest1, indiceBest2;
+	int indiceWorst1, indiceWorst2;
+
+	// pick up the index of the first population to start comparing
+	indiceBest1 = indiceWorst1 = 0;
+
+	//calculate fitness
+	best1 = worst1 = evaluate(indiceBest1);
+
+	// pick up the index of the second population to start comparing
+	indiceBest2 = indiceWorst2 = 1;
+
+	//calculate fitness
+	best2 = worst2 = evaluate(indiceBest2);
+
+	//start a tournament
 	for (int iter = 0; iter < _max_iterations; iter++)
 	{
+		//pull 2 population members at random
 		a = (int)random(1, 10);
 		b = (int)random(1, 10);
-	
+
+		//have a fight, see who has best genes
 		if (evaluate(a) < evaluate(b))
 		{
 			winner = a;
@@ -111,21 +190,23 @@ void GeneticAlgorithmForCardProblemMPI::run(int argc, char * argv[])
 			winner = b;
 			loser = a;
 		}
-
+		//Possibly do some gene jiggling, on all genes of loser
+		//again depends on randomness (simulating the natural selection
+		//process of evolutionary selection)
 		for (int i = 0; i < _dimension; i++)
 		{
 
 			//Crossover operator
-			if (random(0, 2) < _crossover_probability) {
+			if (random(0, 0.5) < _crossover_probability) {
 				_population[loser][i] = _population[winner][i];
 			}
 
 
 			//Mutation operator
-			if (random(0, 2) < _mutation_probability) {
+			if (random(0, 0.5) < _mutation_probability) {
 				_population[loser][i] = 1 - _population[loser][i];
 			}
-
+			//then test to see if the new population member is a winner
 			if (evaluate(loser) == 0.0)
 			{
 				display(iter, loser);
@@ -133,32 +214,21 @@ void GeneticAlgorithmForCardProblemMPI::run(int argc, char * argv[])
 				break;
 			}
 		}		
-		//std::cout << " rank +iter % 20 " << my_rank << "  "<<iter % 20 << std::endl;
 
 		// send 2 best individuals to the next process and receive the 2 best process from the previous process
-		if (iter % 10 == 0) 
+		if (iter % 100 == 0) 
 		{
-			int num_proc_precedent = (num_procs + my_rank - 1) % num_procs;
-			int num_proc_suivant = (my_rank + 1) % num_procs;
-
-			indiceBest1 = indiceWorst1 = 0;
-			best1 = worst1 = evaluate(indiceBest1);
-
-			indiceBest2 = indiceWorst2 = 1;
-			best2 = worst2 = evaluate(indiceBest2);
-
 			//evaluate the 30 individuals and pick up the 2 best and the 2 worst
 			for (int i = 2; i < _popsize; i++)
 			{
+				// get fitness of the current index (i) and test it with the 2 best an 2 worst
 				test = evaluate(i);
-
 				if (test<best2)
 				{
 					if (test<best1)
 					{
 						best2 = best1;
 						best1 = test;
-
 						indiceBest2 = indiceBest1;
 						indiceBest1 = i;
 					}
@@ -167,7 +237,6 @@ void GeneticAlgorithmForCardProblemMPI::run(int argc, char * argv[])
 						best2 = test;
 						indiceBest2 = i;
 					}
-
 				}
 				else
 				{
@@ -190,10 +259,7 @@ void GeneticAlgorithmForCardProblemMPI::run(int argc, char * argv[])
 				}
 
 			}
-			std::cout << " iteration " << iter << std::endl;
-			std::cout << " process numero my_rank " << my_rank << std::endl;
-			std::cout << " evaluate(indiceBest1)  " << evaluate(indiceBest1) << std::endl;
-			std::cout << " evaluate(indiceBest2)  " << evaluate(indiceBest2) << std::endl;
+
 
 			// store the 2 best individuals
 			for (int j = 0; j < 10; j++)
@@ -202,12 +268,13 @@ void GeneticAlgorithmForCardProblemMPI::run(int argc, char * argv[])
 				best_pop2[j] = _population[indiceBest2][j];
 			}
 
-			// store the indice of the tho best indivduals in the last case of each 
+			// store the indice of the tho best indivduals in the last case of each vector
 			best_pop1[10] =indiceBest1;
 			best_pop2[10] = indiceBest2;
 
-			int r;
-			
+
+			// each process send his 2 best population to the next process  
+			// and get the 2 best population of the previous process
 			if (my_rank == 0)
 			{
 				MPI_Send(best_pop1, 11, MPI_INT, num_proc_suivant, tag, MPI_COMM_WORLD);
@@ -223,23 +290,20 @@ void GeneticAlgorithmForCardProblemMPI::run(int argc, char * argv[])
 
 			}
 
-			//replace the 2 worst by the 2 best
+			//each process replace the 2 worst by the 2 best
 			for (int j = 0; j < 10; j++)
 			{
 				_population[indiceWorst1][j] = best_pop1[j];
 				_population[indiceWorst2][j] = best_pop2[j] ;
 
 			}
-
+			//each process pick up the 2 index of the best population for next iteration
 			best1 = best_pop1[10];
-			best2 = best_pop2[10];
-
-			
+			best2 = best_pop2[10];	
 		}
-
-		
-
 	}
+
+	// process number 0 will display the final  result
 	if (my_rank == 0)
 			display(_max_iterations, indiceBest1);
 		
@@ -252,72 +316,3 @@ void GeneticAlgorithmForCardProblemMPI::run(int argc, char * argv[])
 }
 
 
-void GeneticAlgorithmForCardProblemMPI::display(int iteration, int individual)
-{
-	std::cout << "==================================" << std::endl;
-	std::cout << " after " << iteration << " Iterations, Solution sum pile ( should be 36 ) card are :" << std::endl;
-	int sum = 0;
-	int prod = 1;
-	for (int i = 0; i < _dimension; ++i)
-	{
-		
-		if (_population[individual][i] == 0)
-		{
-			std::cout << (i + 1) << "+";
-			std::cout << sum ;
-
-			sum += (i + 1);
-		}
-		std::cout << "=" << sum << std::endl;
-	}
-	std::cout << " and Product pile ( should be 360 ) cards are :" << std::endl;
-
-	for (int i = 0; i < _dimension; ++i)
-	{
-		
-		if (_population[individual][i] == 1)
-		{
-			std::cout << (i + 1) << "*";
-			std::cout << prod;
-			prod *= (i + 1);
-		
-		}
-		std::cout << "=" << prod<< std::endl;
-	}
-
-}
-
-void GeneticAlgorithmForCardProblemMPI::initial_population()
-{
-
-	
-
-	for (int i = 0; i < _popsize; ++i)
-	{
-		for (int j = 0; j < _dimension; ++j)
-		{
-			double x = random(0, 1);
-			
-
-			if (random(0, 2) < 0.5) {
-				_population[i][j] = 0;
-				//std::cout << "_population[" << i << "][" << j << "] " << _population[i][j] << " || ";
-			}
-			else {
-				_population[i][j] = 1;
-				//std::cout << "_population[" << i << "][" << j << "] " << _population[i][j] << " || ";
-			}
-			//std::cout << "random(0, 2) " << x << std::endl;
-			
-		}
-	}
-	//std::cout << "_population[29][9]" << _population[29][9] << std::endl;
-}
-
-double GeneticAlgorithmForCardProblemMPI::random(double min, double max) const
-{
-	
-	double f = (double)rand() / RAND_MAX;
-	return min + f * ( max - min ) ;
-	
-}
--- GeneticAlgorithmForCardProblemMPI.h
+++ GeneticAlgorithmForCardProblemMPI.h
@@ -9,36 +9,68 @@ public:
 
 	GeneticAlgorithmForCardProblemMPI(int maxiteration = 1000, int popsize = 30, int dim = 10,
 		double probCross = 0.5, double probaMut = 0.1,
-		double sumTrag = 36, double prodTrag = 360, int number_of_individual_to_send = 2,
-		int number_of_iteration_before_sending = 100
+		double sumTrag = 36, double prodTrag = 360
 		);
 	
 	~GeneticAlgorithmForCardProblemMPI();
+
 	double random(double min, double max) const;
 
+	//Runs the Microbial GA to solve the problem domain
+	//Where the problem domain is specified as follows
+
+	//You have 10 cards numbered 1 to 10.
+	//You have to divide them into 2 piles so that:
+
+	//The sum of the first pile is as close as possible to 36
+	//And the product of all in second pile is as close as poss to 360
 	void run(int argc, char * argv[]);
 private :
 
+	//population size
 	 int _popsize ;
+	 
+	 //Dimension
 	 int _dimension ;
 
+	 //mutation Probability
 	 double _mutation_probability ;
+
+	 //Crossover probability
 	 double _crossover_probability ;
 
+	 //the genes array, 30 members, 10 cards each
+	 int **  _population;
+
+	 //how many tournaments should be played
 	 int _max_iterations ;
 
+	 // the sum pile, end result for the SUM pile
+	 //card1 + card2 + card3 + card4 + card5, MUST = 36 for a good GA
 	 double _sum_targ ;
+
+	 //the product pile, end result for the PRODUCT pile
+	 //card1 * card2 * card3 * card4 * card5, MUST = 360 for a good GA
 	 double _prod_targ;
 
-	 int **_population ;
-	
-	 int _number_of_individual_to_send ;
+	 //the genes array, 30 members, 10 cards each	 int **_population ;
 	 int _number_of_iteration_before_sending ;
 
-	 
+
+	 //evaluate the the nth member of the population
+	 //@param n : the nth member of the population
+	 //@return : the score for this member of the population.
+	 //If score is 0.0, then we have a good GA which has solved
+	 //the problem domain
 	 double evaluate(int n);
 	 
+
+	 //Display the results. Only called for good GA which has solved
+	 //the problem domain
+	 //@param iteration : the current iteration loop number
+	 //@param n : the nth member of the population.
 	 void display(int iteration, int individual);
+
 	 void initial_population();
 	
 
